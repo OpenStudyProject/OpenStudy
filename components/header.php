4b77ba2c169ec75499329b825070f2be42ff599b
lin24 <53554285+lin-24-ia@users.noreply.github.com>
2023-04-05 20:27:24 +0900

    update: アイコンのsvg化

--- components/header.php
+++ components/header.php
@@ -12,6 +12,25 @@
             <a href=""><li>Menu4</li></a>
         </ul>
 
-        <img src="<?php echo $path ?>images/bars.svg" alt="menuBar" class="sp-menu">
+        <svg class="sp-menu" viewbox="0 0 500 500">
+            <rect
+            width="374.05966"
+            height="33.398186"
+            x="62.970169"
+            y="113.10377"
+            ry="16.699093" />
+            <rect
+            width="374.05966"
+            height="33.398186"
+            x="62.970169"
+            y="233.3009"
+            ry="16.699093" />
+            <rect
+            width="374.05966"
+            height="33.398186"
+            x="62.970169"
+            y="353.49805"
+            ry="16.699093" />
+        </svg>
     </nav>
 </header>
