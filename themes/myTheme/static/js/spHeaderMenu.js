2741d78aee91888b2ead76b039347d9d4affcc5c
lin24 <53554285+lin-24-ia@users.noreply.github.com>
2023-05-16 19:15:20 +0900

    Delete unused function

--- themes/myTheme/static/js/spHeaderMenu.js
+++ themes/myTheme/static/js/spHeaderMenu.js
@@ -1,17 +1,16 @@
-const button = document.getElementById("open-menu");
-const checkbox1 = document.getElementById("abc");
-const checkbox2 = document.getElementById("trigger-checkbox2");
+const button = document.getElementById("js-header-button");
+const headerCheckbox = document.getElementById("js-header-checkbox");
 
 button.addEventListener('click', () => {
-    let flag2 = checkbox2.checked;
-    if (!flag2) {checkbox2.checked = true;}
-    if (flag2) {checkbox2.checked = false;}
-    console.log("2 has checked.");
-})
+    let flag = headerCheckbox.checked;
 
-button.addEventListener('click', () => {
-    let flag1 = checkbox1.checked;
-    if (!flag1) {checkbox1.checked = true;}
-    if (flag1) {checkbox1.checked = false;}
-    console.log("1 has checked.");
+    if (!flag) {
+        headerCheckbox.checked = true;
+        console.log("checked");
+    }
+
+    if (flag) {
+        headerCheckbox.checked = false;
+        console.log("not checked")
+    }
 })
